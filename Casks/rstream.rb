a537f1611f8cc835491671e14723adbbbb0fdbde
CI automation <ci@rstream.io>
2024-01-16 17:00:42 +0000

    update rstream cask to 0.0.33

--- Casks/rstream.rb
+++ Casks/rstream.rb
@@ -8,22 +8,22 @@ cask "rstream" do
   packages = {
     "darwin" => {
       "amd64" => {
-        url: "https://dev.rstream.io/api/packages/65a6ae135f7d560f58c4fbb7/download",
-        sha256: "a0b8dc54c233071fd9da39a0a1a62337081091ef3d74a3f015f1edd576f7141f",
+        url: "https://dev.rstream.io/api/packages/65a6b606d55c0e93a2badd98/download",
+        sha256: "66d1d3342869add2cd5dd83e7459496340d2a1e1e90fc30c082e2de0b09959d5",
       },
       "arm64" => {
-        url: "https://dev.rstream.io/api/packages/65a6ae16dcf046e6abeddf31/download",
-        sha256: "866f6ff5f156d49011716b9295e63c17a5480e5f4b37002509ab870eb8a2de45",
+        url: "https://dev.rstream.io/api/packages/65a6b60a47f987ba1b571cb0/download",
+        sha256: "a10e1258948f7f83fc2a21dd6e32dbe2be241ebd91afa5b9577e1e09e7a62b39",
       }
     },
     "linux" => {
       "amd64" => {
-        url: "https://dev.rstream.io/api/packages/65a6ae1a5f7d560f58c4fbbd/download",
-        sha256: "48993b060b263c26c052358169f1e480b0db1c96c0c021e9b7348957e0dacaf1",
+        url: "https://dev.rstream.io/api/packages/65a6b60d47f987ba1b571cb3/download",
+        sha256: "2de8d8dfb5870c6983da502f5b36d7a7875d9260f81ea4f07598027d0fd82166",
       },
       "arm64" => {
-        url: "https://dev.rstream.io/api/packages/65a6ae1c5f7d560f58c4fbbe/download",
-        sha256: "3b8c60f4a8f7ba02e172c014061b336c4aad36a0314172bec9140acd51a12186",
+        url: "https://dev.rstream.io/api/packages/65a6b60f47f987ba1b571cb4/download",
+        sha256: "c66ddaf1ccb610bad99066650f8e5b97c410caedc877b8b56f42e4e11a3aa9dc",
       }
     }
   }
