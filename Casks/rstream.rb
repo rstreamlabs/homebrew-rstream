6b2f725a8dd8fc2a48ca38462df8b75da3152f9f
CI automation <ci@rstream.io>
2024-01-16 16:14:26 +0000

    update rstream cask to 0.0.32

--- Casks/rstream.rb
+++ Casks/rstream.rb
@@ -2,27 +2,27 @@ cask "rstream" do
   name "rstream"
   desc "secure, scalable, and sovereign networking for the modern enterprise."
   homepage "https://rstream.io"
-  version "0.0.31"
+  version "0.0.32"
   os = OS.mac? ? "darwin" : "linux"
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
   packages = {
     "darwin" => {
       "amd64" => {
-        url: "https://dev.rstream.io/api/packages/65a6a55041a050e92892c458/download",
+        url: "https://dev.rstream.io/api/packages/65a6ab392f635edd114b76e3/download",
         sha256: "",
       },
       "arm64" => {
-        url: "https://dev.rstream.io/api/packages/65a6a5503817e56f66b6b211/download",
+        url: "https://dev.rstream.io/api/packages/65a6ab395f7d560f58c4fba3/download",
         sha256: "",
       }
     },
     "linux" => {
       "amd64" => {
-        url: "https://dev.rstream.io/api/packages/65a6a5553817e56f66b6b215/download",
+        url: "https://dev.rstream.io/api/packages/65a6ab3e5f7d560f58c4fba7/download",
         sha256: "",
       },
       "arm64" => {
-        url: "https://dev.rstream.io/api/packages/65a6a55741a050e92892c45e/download",
+        url: "https://dev.rstream.io/api/packages/65a6ab402f635edd114b76e9/download",
         sha256: "",
       }
     }
