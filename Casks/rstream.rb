d371adbe0c3eca33ca3612963212114ac769a9b2
CI automation <ci@rstream.io>
2024-01-31 06:39:06 +0000

    update rstream cask to 0.0.39

--- Casks/rstream.rb
+++ Casks/rstream.rb
@@ -2,28 +2,28 @@ cask "rstream" do
   name "rstream"
   desc "secure, scalable, and sovereign networking for the modern enterprise."
   homepage "https://rstream.io"
-  version "0.0.37"
+  version "0.0.39"
   os = OS.mac? ? "darwin" : "linux"
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
   packages = {
     "darwin" => {
       "amd64" => {
-        url: "https://dev.rstream.io/api/packages/65b9e608d8ebe7a7dbb4eaba/download",
-        sha256: "ef1f396dffebe2e35a8848f17d37af384adc929c384361383b1aa5c4b22f784b",
+        url: "https://dev.rstream.io/api/packages/65b9eadd65267ac3339ce33c/download",
+        sha256: "2f168f6a3176dd17b65c971df5c92b9431ba362f61f9a1068fb1245e16961df2",
       },
       "arm64" => {
-        url: "https://dev.rstream.io/api/packages/65b9e608e7bb58f63b637e39/download",
-        sha256: "70df0fd7fd7ef63d93d5abc5806957d5050c5b922cdf38745a10294b5c8d0dae",
+        url: "https://dev.rstream.io/api/packages/65b9eadeee18a662bb2979f0/download",
+        sha256: "2714287ba339396c9d3b7fad5dc1f76c584368636824ea3fb16a3261dd550a7d",
       }
     },
     "linux" => {
       "amd64" => {
-        url: "https://dev.rstream.io/api/packages/65b9e60dd8ebe7a7dbb4eabe/download",
-        sha256: "07ea79454eb804aae8b84ca6d2526aa3974aeef85586cfab71bd762087b93368",
+        url: "https://dev.rstream.io/api/packages/65b9eae265267ac3339ce340/download",
+        sha256: "871390a7475b79c659bceb550d9deadd27c7289ee7db6e69edf348f205eb20fb",
       },
       "arm64" => {
-        url: "https://dev.rstream.io/api/packages/65b9e610e7bb58f63b637e3f/download",
-        sha256: "466f88b33b7fbb32c70329fec98b94e25c457a03eab35ff917f922ed062db999",
+        url: "https://dev.rstream.io/api/packages/65b9eae4ee18a662bb2979f6/download",
+        sha256: "f942a28d3d753b46b5a7dc97c76df77314ff0ff7c7dd19efd1fcf6aef69444a0",
       }
     }
   }
